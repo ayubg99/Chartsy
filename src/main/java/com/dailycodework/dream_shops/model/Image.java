7094c08eb6ae377c8d8237ade82377fe7311160c
Ayoub <ayubgharbi@gmail.com>
2025-08-28 20:09:32 +0200

    setting up relationships between models

--- src/main/java/com/dailycodework/dream_shops/model/Image.java
+++ src/main/java/com/dailycodework/dream_shops/model/Image.java
@@ -1,14 +1,26 @@
 package com.dailycodework.dream_shops.model;
 
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.sql.Blob;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String fileName;
     private String fileType;
+
+    @Lob
     private Blob image;
     private String downloadUrl;
 
